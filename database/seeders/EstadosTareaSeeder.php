d37ca21b6ba6b2b9dd906e5b46dfebde51e5486b
garyS2 <alejandrog.ag40@gmail.com>
2025-12-17 06:53:10 -0400

    Agregar migraciones y seeders para proyectos, tareas y tipos de proyecto

---
+++ database/seeders/EstadosTareaSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EstadosTareaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
